d4ff3133ff8a916ce30884d537dcb0bd07ca874d
nguyendu-zoo <nguyenduhaihauc@gmail.com>
2021-11-09 20:30:35 +0700

    update table User

--- Home/models.py
+++ Home/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=15)
-    email = models.EmailField()
-    phonenumber = models.CharField(max_length=12)
-    birthday = models.DateTimeField()
-    date_create = models.DateTimeField(auto_now_add=True)
+    Username = models.CharField(max_length=50)
+    Password = models.CharField(max_length=15)
+    Email = models.EmailField()
+    SDT = models.CharField(max_length=12)
+    NgaySinh = models.DateTimeField()
+    NgayTao = models.DateTimeField(auto_now_add=True)
