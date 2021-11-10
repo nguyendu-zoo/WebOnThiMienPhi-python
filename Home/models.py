822bde04cc0a96f6bf9abfb1026a803d5a817d2d
nguyendu-zoo <nguyenduhaihauc@gmail.com>
2021-11-10 09:38:13 +0700

    Update table User and insert table DeThi

--- Home/models.py
+++ Home/models.py
@@ -9,3 +9,44 @@ class User(models.Model):
     SDT = models.CharField(max_length=12)
     NgaySinh = models.DateTimeField()
     NgayTao = models.DateTimeField(auto_now_add=True)
+    DichVuCaNhan = (
+        (0, "Phụ Huynh"),
+        (1, "Học Sinh"),
+        (2, "Sinh Viên"),
+        (3, "Giáo Viên - Giảng Viên")
+    )
+    ChucVuCaNhan = models.IntegerField(default=0, choices=DichVuCaNhan)
+    GioiThieu = models.TextField()
+ 
+class DeThi(models.Model):
+    TacGia = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    MaDe = models.CharField(max_length=10)
+    TenDe = models.TextField()
+    NgayTao = models.DateTimeField(auto_now_add=True)
+    HinhThucDeThi = (
+        (0, "Luyện tập trắc nghiệm"),
+        (1, "Đề thi thông min"),
+        (2, "Lý thuyết tương tác"),
+        (3, "Hỏi và Đáp"),
+        (4, "Đề thi Tự luận, chấm điểm trực tiếp lên bài"),
+        (5, "Đề thi tắc nghệm từ file PDF hoặc Word"),
+        (6, "Đề thi trắc nghiệm từ ma trận")
+    )
+    HinhThucDe = models.IntegerField(default=0, choices=HinhThucDeThi)
+    ClassForTest = (
+        (0, "Mẫu giáo"),
+        (1, "Lớp 1"),
+        (2, "Lớp 2"),
+        (3, "Lớp 3"),
+        (4, "Lớp 4"),
+        (5, "Lớp 5"),
+        (6, "Lớp 6"),
+        (7, "Lớp 7"),
+        (8, "Lớp 8"),
+        (9, "Lớp 9"),
+        (10, "Lớp 10"),
+        (11, "Lớp 11"),
+        (12, "Lớp 12"),
+        (13, "Đại học")
+    )
+    Lop = models.IntegerField(default=0, choices=ClassForTest)
