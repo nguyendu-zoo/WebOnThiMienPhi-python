20699de5b91635dbfcf0f911ebffe506dc4dd2b3
nguyendu-zoo <nguyenduhaihauc@gmail.com>
2021-11-21 23:49:33 +0700

    Refresh database

--- Home/admin.py
+++ Home/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from .models import User,DeThi,MonThi
+# from django.contrib import admin
+# from .models import User,DeThi,MonThi
 
-# Register your models here.
+# # Register your models here.
 
-admin.site.register(User)
-admin.site.register(DeThi)
-admin.site.register(MonThi)
+# admin.site.register(User)
+# admin.site.register(DeThi)
+# admin.site.register(MonThi)
--- Home/models.py
+++ Home/models.py
@@ -9,18 +9,17 @@ class User(models.Model):
     SDT = models.CharField(max_length=12)
     NgaySinh = models.DateTimeField()
     NgayTao = models.DateTimeField(auto_now_add=True)
-    DichVuCaNhan = (
+    ChucVuOption = (
         (0, "Phụ Huynh"),
         (1, "Học Sinh"),
         (2, "Sinh Viên"),
         (3, "Giáo Viên - Giảng Viên")
     )
-    ChucVuCaNhan = models.IntegerField(default=0, choices=DichVuCaNhan)
-    GioiThieu = models.TextField()
+    ChucVu = models.IntegerField(default=0, choices=ChucVuOption)
+    GioiThieu = models.TextField(null=True)
 
-class MonThi(models.Model):
-    MaMon = models.CharField(max_length=50)
-    MonThiChoices = (
+class MonHoc(models.Model):
+    MonHocOptions = (
         (0, "Tiếng Việt cấp 1"),
         (1, "Tiếng Anh"),
         (2, "Đạo Đức"),
@@ -39,25 +38,11 @@ class MonThi(models.Model):
         (15, "Khoa Học Tự Nhiên"),
         (16, "Sinh Học")
     )
-    TenMon = models.IntegerField(default=0, choices=MonThiChoices)
- 
-class DeThi(models.Model):
-    TacGia = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    TenMonThi = models.ForeignKey(MonThi, on_delete=models.SET_NULL, null=True)
-    MaDe = models.CharField(max_length=10)
-    TenDe = models.TextField()
-    NgayTao = models.DateTimeField(auto_now_add=True)
-    HinhThucDeThi = (
-        (0, "Luyện tập trắc nghiệm"),
-        (1, "Đề thi thông minh"),
-        (2, "Lý thuyết tương tác"),
-        (3, "Hỏi và Đáp"),
-        (4, "Đề thi Tự luận, chấm điểm trực tiếp lên bài"),
-        (5, "Đề thi tắc nghệm từ file PDF hoặc Word"),
-        (6, "Đề thi trắc nghiệm từ ma trận")
-    )
-    HinhThucDe = models.IntegerField(default=0, choices=HinhThucDeThi)
-    ClassForTest = (
+    TenMonHoc = models.IntegerField(default=0, choices=MonHocOptions)
+    GhiChu = models.TextField(null=True)
+
+class Lop(models.Model):
+    LopOptions = (
         (0, "Mẫu giáo"),
         (1, "Lớp 1"),
         (2, "Lớp 2"),
@@ -73,14 +58,92 @@ class DeThi(models.Model):
         (12, "Lớp 12"),
         (13, "Đại học")
     )
-    Lop = models.IntegerField(default=0, choices=ClassForTest)
+    TenLop = models.IntegerField(default=0, choices=LopOptions)
+    
+class HinhThucDeThi(models.Model):
+    HinhThucDeOptions = (
+        (0, "Luyện tập trắc nghiệm"),
+        (1, "Đề thi thông minh"),
+        (2, "Lý thuyết tương tác"),
+        (3, "Hỏi và Đáp"),
+        (4, "Đề thi Tự luận, chấm điểm trực tiếp lên bài"),
+        (5, "Đề thi tắc nghệm từ file PDF hoặc Word"),
+        (6, "Đề thi trắc nghiệm từ ma trận")
+    )
+    HinhThucDe = models.IntegerField(default=0, choices=HinhThucDeOptions)
 
-class DeVaDapAn(models.Model):
-    MaDe = models.ForeignKey(DeThi, on_delete=models.CASCADE)
-    CauHoi = models.TextField()
-    DapAn = models.TextField()
-    CauHoiSo = models.IntegerField(default=0)
+class CauHoi(models.Model):
+    MaDeThi = models.ForeignKey('DeThi', on_delete=models.CASCADE)
+    HinhThucCauHoiOption = (
+        (0, "Trắc nghiệm"),
+        (1, "Tự luận")
+    )
+    HinhThucCauHoi = models.IntegerField(default=0, choices=HinhThucCauHoiOption)
     NoiDungCauHoi = models.TextField()
-    NoiDungCacDapAn = models.TextField()
-    DapAn = models.TextField()
+    DoKhoOption = (
+        (0, "Dễ"),
+        (1, "Trung bình"),
+        (2, "Khó"),
+    )
+    DoKho = models.IntegerField(default=0, choices=DoKhoOption)
+    GhiChu = models.TextField(null=True)
+
+class DapAn(models.Model):
+    CauHoi = models.ForeignKey(CauHoi, on_delete=models.CASCADE)
+    NoiDungDapAn = models.TextField(null=True)
+    DungSai = models.TextField(null=True)
+    GhiChu = models.TextField(null=True)
+ 
+class DeThi(models.Model):
+    MaDeThietLap = models.CharField(max_length=10)
+    TieuDeBaiThi = models.CharField(max_length=200)
+    TacGia = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    TenLopThi = models.ForeignKey(Lop, on_delete=models.SET_NULL, null=True)
+    MonThi = models.ForeignKey(MonHoc, on_delete=models.SET_NULL, null=True)
+    HinhThucThi = models.ForeignKey(HinhThucDeThi, on_delete=models.SET_NULL, null=True)
+    SoLuongCauHoi = models.IntegerField()
+    NgayTao = models.DateTimeField(auto_now_add=True)
+
+# class DeThi(models.Model):
+#     TacGia = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+#     TenMonThi = models.ForeignKey(MonThi, on_delete=models.SET_NULL, null=True)
+#     MaDe = models.CharField(max_length=10)
+#     TenDe = models.TextField()
+#     NgayTao = models.DateTimeField(auto_now_add=True)
+#     HinhThucDeThi = (
+#         (0, "Luyện tập trắc nghiệm"),
+#         (1, "Đề thi thông minh"),
+#         (2, "Lý thuyết tương tác"),
+#         (3, "Hỏi và Đáp"),
+#         (4, "Đề thi Tự luận, chấm điểm trực tiếp lên bài"),
+#         (5, "Đề thi tắc nghệm từ file PDF hoặc Word"),
+#         (6, "Đề thi trắc nghiệm từ ma trận")
+#     )
+#     HinhThucDe = models.IntegerField(default=0, choices=HinhThucDeThi)
+#     ClassForTest = (
+#         (0, "Mẫu giáo"),
+#         (1, "Lớp 1"),
+#         (2, "Lớp 2"),
+#         (3, "Lớp 3"),
+#         (4, "Lớp 4"),
+#         (5, "Lớp 5"),
+#         (6, "Lớp 6"),
+#         (7, "Lớp 7"),
+#         (8, "Lớp 8"),
+#         (9, "Lớp 9"),
+#         (10, "Lớp 10"),
+#         (11, "Lớp 11"),
+#         (12, "Lớp 12"),
+#         (13, "Đại học")
+#     )
+#     Lop = models.IntegerField(default=0, choices=ClassForTest)
+
+# class DeVaDapAn(models.Model):
+#     MaDe = models.ForeignKey(DeThi, on_delete=models.CASCADE)
+#     CauHoi = models.TextField()
+#     DapAn = models.TextField()
+#     CauHoiSo = models.IntegerField(default=0)
+#     NoiDungCauHoi = models.TextField()
+#     NoiDungCacDapAn = models.TextField()
+#     DapAn = models.TextField()
 
