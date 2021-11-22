0a2680e7e05c3ea79b86c9330a1578b5e02d7262
nguyendu-zoo <nguyenduhaihauc@gmail.com>
2021-11-22 09:29:58 +0700

    fix error all class in models.py

--- Home/admin.py
+++ Home/admin.py
@@ -1,8 +1,12 @@
-# from django.contrib import admin
-# from .models import User,DeThi,MonThi
+from django.contrib import admin
+from .models import User, MonHoc, Lop, HinhThucDeThi, CauHoi,DapAn, DeThi
 
 # # Register your models here.
 
-# admin.site.register(User)
-# admin.site.register(DeThi)
-# admin.site.register(MonThi)
+admin.site.register(User)
+admin.site.register(MonHoc)
+admin.site.register(Lop)
+admin.site.register(HinhThucDeThi)
+admin.site.register(CauHoi)
+admin.site.register(DapAn)
+admin.site.register(DeThi)
--- Home/models.py
+++ Home/models.py
@@ -9,13 +9,13 @@ class User(models.Model):
     SDT = models.CharField(max_length=12)
     NgaySinh = models.DateTimeField()
     NgayTao = models.DateTimeField(auto_now_add=True)
-    ChucVuOption = (
-        (0, "Phụ Huynh"),
-        (1, "Học Sinh"),
-        (2, "Sinh Viên"),
-        (3, "Giáo Viên - Giảng Viên")
+    LuaChonChucVu = (
+        ('a', "Phụ Huynh"),
+        ('b', "Học Sinh"),
+        ('c', "Sinh Viên"),
+        ('d', "Giáo Viên - Giảng Viên")
     )
-    ChucVu = models.IntegerField(default=0, choices=ChucVuOption)
+    ChucVu = models.TextField(default='a', choices=LuaChonChucVu)
     GioiThieu = models.TextField(null=True)
 
 class MonHoc(models.Model):
@@ -95,55 +95,11 @@ class DapAn(models.Model):
     GhiChu = models.TextField(null=True)
  
 class DeThi(models.Model):
-    MaDeThietLap = models.CharField(max_length=10)
-    TieuDeBaiThi = models.CharField(max_length=200)
+    TieuDeBaiThi = models.CharField(max_length=200, default="Bài Kiểm Tra Chất Lượng")
     TacGia = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     TenLopThi = models.ForeignKey(Lop, on_delete=models.SET_NULL, null=True)
     MonThi = models.ForeignKey(MonHoc, on_delete=models.SET_NULL, null=True)
     HinhThucThi = models.ForeignKey(HinhThucDeThi, on_delete=models.SET_NULL, null=True)
-    SoLuongCauHoi = models.IntegerField()
+    SoLuongCauHoi = models.IntegerField(default=1)
     NgayTao = models.DateTimeField(auto_now_add=True)
-
-# class DeThi(models.Model):
-#     TacGia = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-#     TenMonThi = models.ForeignKey(MonThi, on_delete=models.SET_NULL, null=True)
-#     MaDe = models.CharField(max_length=10)
-#     TenDe = models.TextField()
-#     NgayTao = models.DateTimeField(auto_now_add=True)
-#     HinhThucDeThi = (
-#         (0, "Luyện tập trắc nghiệm"),
-#         (1, "Đề thi thông minh"),
-#         (2, "Lý thuyết tương tác"),
-#         (3, "Hỏi và Đáp"),
-#         (4, "Đề thi Tự luận, chấm điểm trực tiếp lên bài"),
-#         (5, "Đề thi tắc nghệm từ file PDF hoặc Word"),
-#         (6, "Đề thi trắc nghiệm từ ma trận")
-#     )
-#     HinhThucDe = models.IntegerField(default=0, choices=HinhThucDeThi)
-#     ClassForTest = (
-#         (0, "Mẫu giáo"),
-#         (1, "Lớp 1"),
-#         (2, "Lớp 2"),
-#         (3, "Lớp 3"),
-#         (4, "Lớp 4"),
-#         (5, "Lớp 5"),
-#         (6, "Lớp 6"),
-#         (7, "Lớp 7"),
-#         (8, "Lớp 8"),
-#         (9, "Lớp 9"),
-#         (10, "Lớp 10"),
-#         (11, "Lớp 11"),
-#         (12, "Lớp 12"),
-#         (13, "Đại học")
-#     )
-#     Lop = models.IntegerField(default=0, choices=ClassForTest)
-
-# class DeVaDapAn(models.Model):
-#     MaDe = models.ForeignKey(DeThi, on_delete=models.CASCADE)
-#     CauHoi = models.TextField()
-#     DapAn = models.TextField()
-#     CauHoiSo = models.IntegerField(default=0)
-#     NoiDungCauHoi = models.TextField()
-#     NoiDungCacDapAn = models.TextField()
-#     DapAn = models.TextField()
 
